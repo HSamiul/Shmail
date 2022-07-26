b7860eb71ab8dc5b1aff32b0ce7113b91f681651
HSamiul <samiulh@andrew.cmu.edu>
2022-07-25 23:38:59 -0400

    Initial Commit

---
+++ Shmail/ShmailApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShmailApp.swift
+//  Shmail
+//
+//  Created by Samiul Hoque on 7/25/22.
+//
+
+import SwiftUI
+
+@main
+struct ShmailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
